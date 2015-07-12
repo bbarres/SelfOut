59386d41aa36b99963d9efbe1c024747a4bf264c
bbarres <benoit.barres@gmail.com>
2015-07-12 20:52:42 +0200

    adding the number MLG analysis for 2013 dataset

--- selfout_MLG.R
+++ selfout_MLG.R
@@ -21,7 +21,7 @@ library(SpaceTimeModels)
 
 
 ###############################################################################
-#Factors affecting the number of MLG
+#Factors affecting the number of MLG: 2012
 ###############################################################################
 
 infections <- coinf2012
@@ -35,7 +35,7 @@ complete <- complete.cases(infections[,c("PLM2_Sept2012","number_coinf",
                                          "cumulative_sum","connec2012",
                                          "road_PA","Distance_to_shore",
                                          "PA_2011","number_MLG","AA_F2012")])
-length(complete)
+summary(complete)
 infections <- infections[complete,]
 
 infections$PA_2011 <- as.numeric(infections$PA_2011)
@@ -120,16 +120,6 @@ model$clearStack()$addObservationStack(sp=coords,
 model$estimate()
 model$summary() # WAIC = 1458.88
 
-
-#Full model minus Distance to shore, PA_2011, cumulative_sum, road_PA
-model$setCovariatesModel(~ 1 + PLM2_Sept2012 + AA_F2012 + connec2012, 
-                         covariates=infections)
-model$clearStack()$addObservationStack(sp=coords,
-                                       response=infections$number_MLG,
-                                       covariates=infections)
-model$estimate()
-model$summary() # WAIC = 1454.83
-
 #Model with Absolute Abundance in the fall as fixed effect
 model$setCovariatesModel(~ 1 + AA_F2012, 
                          covariates=infections)
@@ -148,64 +138,219 @@ model$clearStack()$addObservationStack(sp=coords,
 model$estimate()
 model$summary() # WAIC = 1485.47
 
-#Full model minus Distance to shore and PA_2011
-model$setCovariatesModel(~ 1 + road_PA, 
+#Model with Plantago coverage as fixed effect
+model$setCovariatesModel(~ 1 + PLM2_Sept2012, 
                          covariates=infections)
 model$clearStack()$addObservationStack(sp=coords,
                                        response=infections$number_MLG,
                                        covariates=infections)
 model$estimate()
-model$summary() # WAIC = 1457.36
+model$summary() # WAIC = 1480.94
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-model$setCovariatesModel(~ 1 + AA_F2012 + PA_2011, covariates=infections)
-model$clearStack()$addObservationStack(sp=coords, response=infections$number_MLG, covariates=infections)
+#Model with Absolute Abundance of mildew, Plantago coverage and connectivity 
+#as fixed effects. This is the best fitting model with nbinomial likelihood
+#(lowest WAIC)
+model$setCovariatesModel(~ 1 + AA_F2012 + PLM2_Sept2012 + connec2012, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
 model$estimate()
-model$summary() # WAIC = 1458.94
+model$summary() # WAIC = 1454.83
 
-# Best fitting model with nbinomial likelihood (lowest WAIC)
-model$setCovariatesModel(~ 1 + PLM2_Sept2012 + AA_F2012, covariates=infections)
-model$clearStack()$addObservationStack(sp=coords, response=infections$number_MLG, covariates=infections)
-model$estimate()
-model$summary() # WAIC = 1455.63
-fittedResponse <- model$getFittedResponse()
-table(infections$number_MLG)
-table(round(fittedResponse$responseMean))
-
-model$setCovariatesModel(~ 1 + AA_F2012, covariates=infections)
-model$clearStack()$addObservationStack(sp=coords, response=infections$number_MLG, covariates=infections)
-model$estimate()
-model$summary() # WAIC = 1457.46
 
 # There is no overdispersion, so poisson likelihood is enough
 model$setLikelihood("poisson")
-model$setCovariatesModel(~ 1 + PLM2_Sept2012 + AA_F2012, covariates=infections)
-model$clearStack()$addObservationStack(sp=coords, response=infections$number_MLG, covariates=infections)
+model$setCovariatesModel(~ 1 + AA_F2012 + PLM2_Sept2012 + connec2012, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
 model$estimate()
-model$summary() # WAIC = 1424.59
+model$summary() # WAIC = 1423.84
+
+fittedResponse <- model$getFittedResponse()
+table(infections$number_MLG)
+table(trunc(fittedResponse$responseMean))
+
+# Non-spatial models
+nonSpatmod<-inla(number_MLG ~ 1, data=infections, family="poisson", 
+                 control.predictor=list(compute=TRUE), 
+                 control.compute=list(waic=TRUE))
+summary(nonSpatmod) # WAIC = 1461.06
+nonSpatmodBest<-inla(number_MLG ~ 1 + AA_F2012 + PLM2_Sept2012 + connec2012, 
+                     data=infections, family="poisson", 
+                     control.predictor=list(compute=TRUE), 
+                     control.compute=list(waic=TRUE))
+summary(nonSpatmodBest) # WAIC = 1423.98
+
+# The spatial model appears not to improve the fit much
+table(round(nonSpatmodBest$summary.fitted.values$mean))
+
+
+###############################################################################
+#Factors affecting the number of MLG: 2013
+###############################################################################
+
+infections <- coinf2013
+#patches with coinfected sampled should have at list 2 different MLG, 
+#we therefore apply a correction to the numnber of MLG for patches 
+#with coinfection but with less than 2 single identified MLG
+infections[infections$number_coinf>0 & infections$number_MLG<2,]$number_MLG<-2
+
+# Remove missing data and scale covariates
+complete <- complete.cases(infections[,c("PLM2_Sept2013","number_coinf",
+                                         "cumulative_sum","connec2013",
+                                         "road_PA","Distance_to_shore",
+                                         "PA_2012","number_MLG","AA_F2013")])
+summary(complete)
+infections <- infections[complete,]
+
+infections$PA_2012 <- as.numeric(infections$PA_2012)
+infections[,c("PLM2_Sept2013","number_coinf",
+              "cumulative_sum","connec2013",
+              "road_PA","Distance_to_shore",
+              "PA_2012","AA_F2013")]<-infections[,c("PLM2_Sept2013",
+                                                    "number_coinf",
+                                                    "cumulative_sum",
+                                                    "connec2013","road_PA",
+                                                    "Distance_to_shore",
+                                                    "PA_2012","AA_F2013")]
+coords <- sp::SpatialPoints(infections[,c("Longitude","Latitude")])
+#infections$number_MLG <- infections$number_MLG - 1
+
+#Construct estimation mesh
+mesh <- SpaceTimeModels::NonConvexHullMesh$new(knots=coords,knotsScale=1e5, 
+                                               cutoff=1e3,maxEdge=c(2.2e3,1e5),
+                                               convex=0.1)
+mesh$getINLAMesh()$n
+mesh$plot()
+
+#Setup spatial model
+model<-ContinuousSpaceModel$new()
+model$setSpatialMesh(mesh)
+model$setSpatialPrior()
+model$setLikelihood("nbinomial")
+
+#Intercept-only model
+model$setSmoothingModel()
+model$addObservationStack(sp=coords, response=infections$number_MLG)
+model$estimate()
+model$summary() # WAIC = 1756.63
+
+#Full model
+model$setCovariatesModel(~ 1 + PLM2_Sept2013 + AA_F2013 + Distance_to_shore + 
+                           PA_2012 + cumulative_sum + connec2013 + road_PA, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
+model$estimate()
+model$summary() # WAIC = 1720.98
+
+#Full model minus PA_2012
+model$setCovariatesModel(~ 1 + PLM2_Sept2013 + AA_F2013 + Distance_to_shore + 
+                           cumulative_sum + connec2013 + road_PA, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
+model$estimate()
+model$summary() # WAIC = 1720.56
+
+#Full model minus Distance_to_shore
+model$setCovariatesModel(~ 1 + PLM2_Sept2013 + AA_F2013 + 
+                           PA_2012 + cumulative_sum + connec2013 + road_PA, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
+model$estimate()
+model$summary() # WAIC = 1719.90
+
+#Full model minus cumulative_sum
+model$setCovariatesModel(~ 1 + PLM2_Sept2013 + AA_F2013 + Distance_to_shore + 
+                           PA_2012 + connec2013 + road_PA, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
+model$estimate()
+model$summary() # WAIC = 1719.64
+
+#Full model minus road_PA
+model$setCovariatesModel(~ 1 + PLM2_Sept2013 + AA_F2013 + Distance_to_shore + 
+                           PA_2012 + cumulative_sum + connec2013, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
+model$estimate()
+model$summary() # WAIC = 1719.87
+
+#Model with Absolute Abundance in the fall as fixed effect
+model$setCovariatesModel(~ 1 + AA_F2013, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
+model$estimate()
+model$summary() # WAIC = 1717.08
+
+#Model with Pathogen connectivity as fixed effect
+model$setCovariatesModel(~ 1 + connec2013, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
+model$estimate()
+model$summary() # WAIC = 1757.32 BAD
+
+#Model with Plantago coverage as fixed effect
+model$setCovariatesModel(~ 1 + PLM2_Sept2013, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
+model$estimate()
+model$summary() # WAIC = 1757.54 BAD
+
+#Model with Absolute Abundance of mildew and connectivity as fixed effects. 
+#This is the best fitting model with nbinomial likelihood (lowest WAIC)
+model$setCovariatesModel(~ 1 + AA_F2013 + connec2013, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
+model$estimate()
+model$summary() # WAIC = 1716.91
+
+
+# There is no overdispersion, so poisson likelihood is enough
+model$setLikelihood("poisson")
+model$setCovariatesModel(~ 1 + AA_F2013 + connec2013, 
+                         covariates=infections)
+model$clearStack()$addObservationStack(sp=coords,
+                                       response=infections$number_MLG,
+                                       covariates=infections)
+model$estimate()
+model$summary() # WAIC = 1679.34
 
 fittedResponse <- model$getFittedResponse()
 table(infections$number_MLG)
 table(round(fittedResponse$responseMean))
 
 # Non-spatial models
-summary(inla(number_MLG ~ 1, data=infections, family="poisson", control.predictor=list(compute=TRUE), control.compute=list(waic=TRUE)))
-# WAIC = 1461.06
-fit <- inla(number_MLG ~ 1 + PLM2_Sept2012 + AA_F2012, data=infections, family="poisson", control.predictor=list(compute=TRUE), control.compute=list(waic=TRUE))
-summary(fit)
-# WAIC = 1425.23
+nonSpatmod<-inla(number_MLG ~ 1, data=infections, family="poisson", 
+                 control.predictor=list(compute=TRUE), 
+                 control.compute=list(waic=TRUE))
+summary(nonSpatmod) # WAIC = 1720.61
+nonSpatmodBest<-inla(number_MLG ~ 1 + AA_F2013 + connec2013, 
+                     data=infections, family="poisson", 
+                     control.predictor=list(compute=TRUE), 
+                     control.compute=list(waic=TRUE))
+summary(nonSpatmodBest) # WAIC = 1678.58
+
 # The spatial model appears not to improve the fit much
-table(round(fit$summary.fitted.values$mean))
+table(round(nonSpatmodBest$summary.fitted.values$mean))
+
